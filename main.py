f050cd084b482597df1b8d15362ed01b999c0a8b
flymolo42 <flymolo@gmail.com>
2025-07-06 14:36:09 -0500

    pygame init

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+from constants import *
+def main():
+    print("Starting Asteroids!")
+    print("Screen width:",SCREEN_WIDTH)
+    print("Screen height:",SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        return
+
+if __name__ == "__main__":
+    main()
