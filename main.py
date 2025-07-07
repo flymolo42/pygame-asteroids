36cf1471d7aeca34b425fdb829565ce0e1e5b96f
flymolo42 <flymolo@gmail.com>
2025-07-07 10:36:34 -0500

    game clock set up

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock=pygame.time.Clock()
+    dt=0
     while True:
         screen.fill((0,0,0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        dt=game_clock.tick(60)/1000
 if __name__ == "__main__":
     main()
