58e34370439c3a40cc1c096bb67c5b6d194d142e
flymolo42 <flymolo@gmail.com>
2025-07-07 13:27:27 -0500

    Player movement done

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
     dt=0
     while True:
         screen.fill(pygame.Color(0,0,0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
