cf7af8c1ebc05086769d46277ff5ae3937ffd5fc
flymolo42 <flymolo@gmail.com>
2025-07-07 10:32:09 -0500

    black screen window close works

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ def main():
     print("Starting Asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
+    pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-        return
+    while True:
+        screen.fill((0,0,0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 if __name__ == "__main__":
     main()
