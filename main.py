40a39a22858e4f76c3be861d8659764920fca9f2
flymolo42 <flymolo@gmail.com>
2025-07-07 15:05:27 -0500

    asteroids no collisions yet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
     print("Starting Asteroids!")
     print("Screen width:",SCREEN_WIDTH)
@@ -12,9 +15,12 @@ def main():
     dt=0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers=(updatable,drawable)
+    Asteroid.containers=(updatable,drawable,asteroids)
+    AsteroidField.containers=(updatable)
     player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-
+    asteroidField=AsteroidField()
     while True:
         screen.fill(pygame.Color(0,0,0))
         updatable.update(dt)
